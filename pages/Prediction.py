0270993c4cddf9cd1127d7c9e451bd9120210c20
adr-denzel <denzel.ramsbottom@gmail.com>
2023-04-23 15:56:28 +0200

    Steps

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -1,3 +1,4 @@
+
 import pickle
 import streamlit as st
 from PIL import Image
@@ -11,6 +12,7 @@ st.title("Predicting a Churner")
 
 image = Image.open(
     'images/investment-forecast-or-prediction-vision-to-see-investing-opportunity-future-profit-from-stock-and-crypto-trading-concept-flat-modern-illustration-vector.jpg')
+
 st.image(image,
          caption='Source: https://static.vecteezy.com/system/resources/previews/014/563/665/original/investment-forecast-or-prediction-vision-to-see-investing-opportunity-future-profit-from-stock-and-crypto-trading-concept-flat-modern-illustration-vector.jpg',
          use_column_width=True)
@@ -23,21 +25,24 @@ dependent_count = st.sidebar.slider('Dependent Count:', min_value=0, max_value=5
 education_level = st.sidebar.selectbox('Education Level:',
                                        ['Uneducated', 'High School', 'College', 'Graduate', 'Post-Graduate',
                                         'Doctorate', 'Unknown'])
+
 marital_status = st.sidebar.selectbox('Marital Status:', ['Single', 'Married', 'Divorced', 'Unknown'])
 income_category = st.sidebar.selectbox('Income Category:',
                                        ['Less than $40K', '$40K - $60K', '$80K - $120K', '$60K - $80K', '$120K +',
                                         'Unknown'])
+
 card_category = st.sidebar.selectbox('Credit Card Product:', ['Blue', 'Silver', 'Gold', 'Platinum'])
-months_on_book = st.sidebar.slider('Months on Book:', min_value=0, max_value=60, value=0)
+months_on_book = st.sidebar.slider('Months on Book:', min_value=12, max_value=60, value=12)
 relationship_count = st.sidebar.slider('Relationship Count:', min_value=1, max_value=6, value=1)
 months_inactive_12_mon = st.sidebar.slider('Months Inactive Over Past Year', min_value=0, max_value=6, value=2)
 contact_count_12_mon = st.sidebar.slider('Contact Count Over Past Year', min_value=0, max_value=6, value=2)
-credit_limit = st.sidebar.slider('Credit Limit:', min_value=1000, max_value=35000, value=1000)
-total_revolving_balance = st.sidebar.slider('Card Balance:', min_value=0, max_value=35000, value=1000)
-total_trans_amount = st.sidebar.slider('Total Transactions Value:', min_value=0, max_value=35000, value=5000)
+credit_limit = st.sidebar.slider('Credit Limit:', min_value=1000, max_value=35000, value=1000, step=1000)
+total_revolving_balance = st.sidebar.slider('Card Balance:', min_value=0, max_value=35000, value=1000, step=1000)
+total_trans_amount = st.sidebar.slider('Total Transactions Value:', min_value=0, max_value=35000, value=5000, step=1000)
 total_trans_count = st.sidebar.slider('Total Transactions Count:', min_value=0, max_value=150, value=50)
 total_amt_change_q4_q1 = st.sidebar.slider('Total Transactions Value Change Change Q4 to Q1:', min_value=0,
                                            max_value=4, value=1)
+
 total_ct_change_q4_q1 = st.sidebar.slider('Total Transactions Count Change Q4 to Q1:', min_value=0, max_value=4,
                                           value=1)
 
